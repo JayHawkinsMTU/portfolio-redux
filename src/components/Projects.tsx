462f133e0ff3f5473c365470786cf0796e6d58ab
Jay <hawkinsjr27@gmail.com>
2025-08-29 14:34:01 -0400

    chore: use overflow instead of masking for tv

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,9 +4,7 @@ import noise from '@/assets/256x256_Dissolve_Noise_Texture.png'
 export default function Projects() {
   return (
     <div class={styles.tv}>
-      <div class={styles.mask}>
-        <img src={noise} alt="tv static"/>
-      </div>
+      <img src={noise} alt="tv static"/>
     </div>
   )
 }
