966203932c7ae9915b9d76f94afaa862764ad7c7
Jay <hawkinsjr27@gmail.com>
2025-08-29 15:03:12 -0400

    fix: better tv scaling

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,7 @@ import noise from '@/assets/256x256_Dissolve_Noise_Texture.png'
 export default function Projects() {
   return (
     <div class={styles.tv}>
-      <img src={noise} alt="tv static"/>
+      <div class={styles.noise}/>
     </div>
   )
 }
